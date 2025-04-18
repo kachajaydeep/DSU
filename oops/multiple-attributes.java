14b905977ebf1c703b0cd1e7ee5793ca86e88dca
Jaydeep Kacha <jaydeepkacha8903@gmail.com>
2025-04-18 19:59:59 +0530

    start

---
+++ oops/multiple-attributes.java
@@ -0,0 +1,13 @@
+public class Main{
+ String fname = "John";
+ String lname = "Doe";
+ int age = 24;
+ 
+ 
+ public static void main(String[] args){
+     
+     Main myObj1 = new Main();
+     System.out.println(myObj1.fname + " " + myObj1.lname);
+ }
+ 
+}
